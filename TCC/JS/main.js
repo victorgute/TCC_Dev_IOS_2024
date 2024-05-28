5d62b4aca2c4e6bf7721689ada63a33a0a7b43fc
victor <victor.h.gutierrez.carvalho>
2024-05-28 17:15:35 -0300

    quase

--- TCC/JS/main.js
+++ TCC/JS/main.js
@@ -1,8 +1,2 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuButton = document.querySelector(".navbar img.tresbarras");
-    const menuItems = document.querySelector(".navbar ul");
 
-    menuButton.addEventListener("click", function() {
-        menuItems.classList.toggle("active");
-    });
-});
+
