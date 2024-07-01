d58938e80a56bf18a7a9f2ac0a80d45d46c89fb5
victor <victor.h.gutierrez.carvalho>
2024-06-30 23:03:38 -0300

    hfjksadhfjkhws

--- JS/produtos.js
+++ JS/produtos.js
@@ -126,3 +126,17 @@ function filtrarPorPreco(min, max) {
         }
     });
 }
+
+function filtrarPorNome() {
+    const input = document.getElementById('searchInput').value.toLowerCase();
+    const produtos = document.querySelectorAll('.produto');
+    produtos.forEach(produto => {
+        const nomeProduto = produto.querySelector('.produto-nome').textContent.toLowerCase();
+        if (nomeProduto.includes(input)) {
+            produto.style.display = 'block';
+        } else {
+            produto.style.display = 'none';
+        }
+    });
+}
+
